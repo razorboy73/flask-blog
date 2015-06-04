a6936ef979da357023c87a65777302a41b471b5d
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-06-04 13:19:24 -0400

    added basic password hashing

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 __author__ = 'workhorse'
 
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
@@ -16,7 +17,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return "<user %r>"%self.username
 
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash, password)
