fd555655c4cc160e212519a7493ef15e53f1d819
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-06-05 18:03:41 -0400

    confirmation token generation

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ __author__ = 'workhorse'
 from app import db
 from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from . import login_manager
 
 class Role(db.Model):
@@ -20,6 +22,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return "<user %r>"%self.username
@@ -38,3 +41,22 @@ class User(db.Model, UserMixin):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({"confirm": self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+
