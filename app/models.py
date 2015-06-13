b732bc9e4503fac51d8788c36b841ea7644f8344
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-06-13 12:28:01 -0400

    Implemented Role Assignment

--- app/models.py
+++ app/models.py
@@ -103,4 +103,10 @@ class User(db.Model, UserMixin):
         db.session.add(self)
         return True
 
-
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email ==current_app.config["FLASKY_ADMIN"]:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
