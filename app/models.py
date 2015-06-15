95cfa0da3c65378b24e068144ef8979940120614
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-06-15 14:41:36 -0400

    added Avatars

--- app/models.py
+++ app/models.py
@@ -4,9 +4,11 @@ from app import db
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from . import login_manager
 from datetime import datetime
+import hashlib
+
 
 class Role(db.Model):
     __tablename__= 'roles'
@@ -109,6 +111,25 @@ class User(db.Model, UserMixin):
         db.session.add(self)
         return True
 
+    def generate_email_change_token(self,new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({"change_email":self.id,"new_email":new_email})
+
+    def change_email(self,token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -129,6 +150,13 @@ class User(db.Model, UserMixin):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
 
+    def gravatar(self, size=100, default="identicon", rating="g"):
+        if request.is_secure:
+            url = "https://secure.gravatar.com/avatar"
+        else:
+            url = "http://www.gravatar.com/avatar"
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
