4553cf7b901b01170520bda54c1c5296ccb8b41a
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-06-16 21:01:28 -0400

    Post_links: Permalinks added

--- app/main/views.py
+++ app/main/views.py
@@ -25,7 +25,10 @@ def index():
     posts = pagination.items
     return render_template("index.html", form=form, posts=posts, pagination=pagination, current_time=datetime.now())
 
-
+@main.route("/post/<int:id>")
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template("post.html",posts=[post])
 
 @main.route("/admin")
 @login_required
